e8f6cbf76bf4ba72c62593633bc2922e27aa6667
bigbrother13 <gritskevich-91@mail.ru>
2018-06-03 18:55:55 +0300

    Refactor code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-
   def bootstrap_class_for(name)
-    { success:"alert_succcess",
-      error:"alert_danger",
-      danger:"alert_danger",
-      alert:"alert_warning",
-      notice:"alert_info",
+    {
+      success: "alert_succcess",
+      error: "alert_danger",
+      danger: "alert_danger",
+      alert: "alert_warning",
+      notice: "alert_info",
     }[name.to_sym] || name
   end
 end
